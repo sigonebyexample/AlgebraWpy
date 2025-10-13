0263809f0e97f87dc73d8d546806c99a6e4c7676
sigonebyexample <hosseinnansaei@gmail.com>
2025-10-13 23:52:13 +0330

    Update Code2.py

--- Code2.py
+++ Code2.py
@@ -1,1 +1,7 @@
-
+import sympy
+from sympy import symbols
+from sympy.solvers import solve
+x = symbols('x')
+eq = input("Enter your equation (in terms of x): ")
+solve(eq,x)
+print("x= ", solve(eq,x))
