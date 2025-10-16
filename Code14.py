a60b45844eb133c7bfecff212d729cde300918b0
sigonebyexample <hosseinnansaei@gmail.com>
2025-10-16 17:58:04 +0330

    Add files via upload

---
+++ Code14.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+fig, ax= plt.subplots()
+xmin= -10
+xmax= 10
+ymin= -10
+ymax= 10
+plt.axis([xmin,xmax,ymin,ymax])
+plt.plot([xmin,xmax],[0,0],'b')
+plt.plot([0,0],[ymin,ymax],'b')
+plt.show()
