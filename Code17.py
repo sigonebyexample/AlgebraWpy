5bdaeccef7fbbf278c967bd13bd4407c6dc383e3
sigonebyexample <hosseinnansaei@gmail.com>
2025-10-22 23:36:58 +0330

    Add files via upload

---
+++ Code17.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+fig, ax= plt.subplots()
+xmin= -10
+xmax= 10
+ymin= -10
+ymax= 10
+plt.axis([xmin,xmax,ymin,ymax])
+plt.plot([xmin,xmax],[0,0],'b')
+plt.plot([0,0],[ymin,ymax],'b')
+print("x \t y")
+for x in range(10):
+    y = 0.5*x + 1
+    plt.plot([x],[y],'ro')
+    print(x, "\t", y)
+plt.show()
